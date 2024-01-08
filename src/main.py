97fa2ea90e45b06f66f6cfc65266b5b9e4424f1a
GEORGE MATTA <jojomedhat2004@gmail.com>
2024-01-08 15:44:57 -0800

    revert attempts to iriginal trial numbers

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from solver import Solver, attempt
 
-attempt(25, Solver.hill_climb)
-attempt(5, Solver.simulated_annealing)
-attempt(15, Solver.genetic_algorithm)
-attempt(5, Solver.min_conflicts)
+attempt(500, Solver.hill_climb)
+attempt(10, Solver.simulated_annealing)
+attempt(500, Solver.genetic_algorithm)
+attempt(500, Solver.min_conflicts)

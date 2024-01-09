5204b276c2d8b2ad5bc273b8c76e0ac554214c9b
GEORGE MATTA <jojomedhat2004@gmail.com>
2024-01-08 16:02:16 -0800

    mixed up sim-a and ga

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from solver import Solver, attempt
 
 attempt(500, Solver.hill_climb)
-attempt(10, Solver.simulated_annealing)
-attempt(500, Solver.genetic_algorithm)
+attempt(500, Solver.simulated_annealing)
+attempt(10, Solver.genetic_algorithm)
 attempt(500, Solver.min_conflicts)
